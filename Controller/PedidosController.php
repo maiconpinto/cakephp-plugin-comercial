74f9106f69f6ada303eef8bb30b4700e5fb597fb
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-03-17 17:55:35 -0300

    Save Pedido

--- Controller/PedidosController.php
+++ Controller/PedidosController.php
@@ -4,6 +4,14 @@ class PedidosController extends ComercialAppController
 {
     public function novo() 
     {
-        
+        if ($this->request->is('post')) {
+            $this->Pedido->create();
+
+            if ($this->Pedido->save($this->request->data)) {
+                $this->Flash->success('Informe os produtos deste pedido');
+                return $this->redirect(array('action' => 'produtos', $this->Pedido->id));
+            }
+            $this->Flash->error('Não foi possível cadastrar seu pedido, tente novamente.');
+        }
     }
 }
