116bc10a216fb6de13132c105695432eb58a728e
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-03-17 15:39:57 -0300

    Add ComercialAppController

--- Controller/ComercialAppController.php
+++ Controller/ComercialAppController.php
@@ -4,4 +4,12 @@ App::uses('AppController', 'Controller');
 
 class ComercialAppController extends AppController {
 
+    public function isAuthorized($user)
+    {
+        if ($this->Auth->login()) {
+            return true;
+        }
+
+        return false;
+    }
 }
