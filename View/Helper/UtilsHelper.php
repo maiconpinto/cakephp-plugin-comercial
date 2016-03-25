85b5cb1ae0625e31724be9fbfe5d7923e7f680b5
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-03-25 09:22:52 -0300

    Add function itemValorTotal to UtilsHelper

--- View/Helper/UtilsHelper.php
+++ View/Helper/UtilsHelper.php
@@ -20,11 +20,26 @@ class UtilsHelper extends Helper
         return number_format($valor, 2, ',', '.');
     }
 
-    function getValor($valor = null) {
+    public function getValor($valor = null) {
         if (empty($valor)) {
             return 0;
         }
 
         return str_replace(array(',','.'), array('',','), $valor);
+    }
+
+    public function itemValorTotal($qtde = null, $valor_unitario = null, $valor_total)
+    {
+        if (empty($qtde) || empty($valor_unitario) || empty($valor_total)) {
+            return 'Valor inválido';
+        }
+
+        $valor_total_calculado = $qtde * $valor_unitario;
+
+        if ($valor_total_calculado != $valor_total) {
+            return "R$ {$this->moeda_de_db($valor_total)} <small style='color: red; text-decoration:line-through'>R$ {$this->moeda_de_db($valor_total_calculado)}</small>";
+        }
+
+        return "R$ " . $this->moeda_de_db($valor_total);
     }
 }
