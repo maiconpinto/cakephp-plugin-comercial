1c208975503be15d32b4307e904b83c7297511b4
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-05-01 10:35:00 -0300

    Change status, add confirmados

--- Controller/ComercialController.php
+++ Controller/ComercialController.php
@@ -9,11 +9,11 @@ class ComercialController extends ComercialAppController
     public function index()
     {
         $pedidos = $this->Pedido->find('count');
-        $pendentes = $this->Pedido->find('count', array('conditions' => array('Pedido.status' => '1')));
-        $andamento = $this->Pedido->find('count', array('conditions' => array('Pedido.status' => '2')));
-        $aguardando = $this->Pedido->find('count', array('conditions' => array('Pedido.status' => '3')));
+        $andamento = $this->Pedido->find('count', array('conditions' => array('Pedido.status' => '1')));
+        $aguardando = $this->Pedido->find('count', array('conditions' => array('Pedido.status' => '2')));
+        $confirmados = $this->Pedido->find('count', array('conditions' => array('Pedido.status' => '3')));
         
-        $this->set(compact('pedidos', 'pendentes', 'andamento', 'aguardando'));
+        $this->set(compact('pedidos', 'andamento', 'aguardando', 'confirmados'));
     }
 
     public function pedidos()
@@ -22,22 +22,22 @@ class ComercialController extends ComercialAppController
 
         $this->set(compact('pedidos'));
     }
-
-    public function pendentes()
+    
+    public function andamento()
     {
         $pedidos = $this->Pedido->find('all', array('conditions' => array('Pedido.status' => '1')));
         
         $this->set(compact('pedidos'));
     }
 
-    public function andamento()
+    public function aguardando()
     {
         $pedidos = $this->Pedido->find('all', array('conditions' => array('Pedido.status' => '2')));
         
         $this->set(compact('pedidos'));
     }
-
-    public function aguardando()
+    
+    public function confirmados()
     {
         $pedidos = $this->Pedido->find('all', array('conditions' => array('Pedido.status' => '3')));
         
