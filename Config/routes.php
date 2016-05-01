7beabad511ff295e289e239f780c82f3a3e546b1
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-05-01 10:33:38 -0300

    Uupdate Routes, remove /pendentes

--- Config/routes.php
+++ Config/routes.php
@@ -3,6 +3,6 @@
 Router::connect('/comercial', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'index'));
 
 Router::connect('/pedidos', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'pedidos'));
-Router::connect('/pendentes', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'pendentes'));
 Router::connect('/andamento', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'andamento'));
 Router::connect('/aguardando', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'aguardando'));
+Router::connect('/confirmados', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'confirmados'));
