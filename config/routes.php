374674cad24d1f6ed8ff082e2ebc2f8c718d951b
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-06-29 16:13:59 -0300

    Fix routes

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,9 @@
 <?php
-use Cake\Core\Plugin;
-use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
-Router::connect('/comercial', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'index'));
+Router::connect('/comercial', array('plugin' => 'PluginComercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'index'));
 
-Router::connect('/pedidos', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'pedidos'));
-Router::connect('/andamento', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'andamento'));
-Router::connect('/aguardando', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'aguardando'));
-Router::connect('/confirmados', array('plugin' => 'comercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'confirmados'));
+Router::connect('/pedidos', array('plugin' => 'PluginComercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'pedidos'));
+Router::connect('/andamento', array('plugin' => 'PluginComercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'andamento'));
+Router::connect('/aguardando', array('plugin' => 'PluginComercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'aguardando'));
+Router::connect('/confirmados', array('plugin' => 'PluginComercial', 'admin' => false,  'controller' => 'comercial', 'action' => 'confirmados'));
